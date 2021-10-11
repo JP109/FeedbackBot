86b57eb57708eb1f2b2c5d11890854f7fed28235
JP109 <jaipawar113@gmail.com>
2021-10-11 18:47:12 +0530

    FeedbackBot base functionality: Sentiment classification

---
+++ chatgui.py
@@ -0,0 +1,69 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+model = pickle.load(open('trial_model.sav', 'rb'))
+
+def chatbot_response(msg):
+    if msg=='Hi':
+        return 'Hello! Did you like the movie? We would love to hear your thoughts'
+    else:
+        res = model.predict([msg])
+        if res[0]==0:
+            return 'We apologize for the inconvinience, you can contact customer support for further details at 20198198/customersupport@gmail.com'
+        else:
+            return 'Glad you liked it! You might also like: The Matrix, Shawshank redemption'
+
+
+#Creating GUI with tkinter
+from tkinter import *
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    EntryBox.delete("0.0",END)
+
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+    
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+            
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+ 
+
+base = Tk()
+base.title("Hello")
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+
+#Create Chat window
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+
+ChatLog.config(state=DISABLED)
+
+#Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+#Create Button to send message
+SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                    command= send )
+
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+#EntryBox.bind("<Return>", send)
+
+
+#Place all components on the screen
+scrollbar.place(x=376,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+base.mainloop()
